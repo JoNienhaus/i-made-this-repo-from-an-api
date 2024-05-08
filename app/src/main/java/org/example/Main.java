ed3f3b37b9b6f824077ebdfe8c50799f935ef9a2
Jonas Nienhaus <j.nienhaus@reanmo.com>
2024-05-08 08:56:14 +0200

    changes

--- app/src/main/java/org/example/Main.java
+++ app/src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ public class Main
 	{
 		//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
 		// to see how IntelliJ IDEA suggests fixing it.
-		System.out.print("Hello and welcome!");
+		System.out.print("Hello!");
 
 		for (int i = 1; i <= 5; i++)
 		{
